fcf2a8be4d8f5290f8ebff78e3d3b924342a62b0
Q07K <kgh0730@gmail.com>
2025-03-13 03:25:53 +0900

    Feat: import models

--- create_table.py
+++ create_table.py
@@ -1,21 +1,11 @@
 from dotenv import load_dotenv
 
 from app.database.postgresql import Base, engine
-from app.models import (
-    chat_message_model,
-    chat_model,
-    session_model,
-    user_model,
-)
+from app.models import *
 
 
 def create_table():
     load_dotenv()
-
-    chat_model.ChatModel
-    chat_message_model.ChatMessageModel
-    session_model.SessionModel
-    user_model.UserModel
 
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
