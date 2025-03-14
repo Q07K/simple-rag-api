1ece0cd5560cc059a4f555e57e3d2bbbd2b81900
gyuhyeong <kgh0730@gmail.com>
2025-03-15 02:31:49 +0900

    Feat: Include `auth_router`

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI, HTTPException
 
+from app.routers import auth_router
 from app.services import custom_exception_response
 
 app = FastAPI()
 
+# Exception Handler
 app.add_exception_handler(
-    HTTPException,
-    custom_exception_response.custom_exception_response,
+    exc_class_or_status_code=HTTPException,
+    handler=custom_exception_response.custom_exception_response,
 )
+
+# Include Router
+app.include_router(router=auth_router.router)
 
 
 @app.get("/{test}")
