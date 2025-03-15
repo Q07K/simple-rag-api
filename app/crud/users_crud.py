87a8a9114a374ad3674738ac8706df59bb76ffe3
gyuhyeong <kgh0730@gmail.com>
2025-03-15 15:33:05 +0900

    Feat: user create

--- app/crud/users_crud.py
+++ app/crud/users_crud.py
@@ -6,6 +6,16 @@ from app.models import *
 from app.models import UserModel
 
 
+def create_user(
+    db: Session,
+    email: str,
+    hashed_password: str,
+):
+    model = UserModel(email=email, hashed_passwor=hashed_password)
+    db.add(model)
+    db.commit()
+
+
 def get_user_by_email(
     db: Session,
     email: str,
