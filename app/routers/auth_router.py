2c2f32149d43d0885172faae16161bb927d5be2e
gyuhyeong <kgh0730@gmail.com>
2025-03-15 14:28:57 +0900

    Feat: Logout API 구현

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
+from app.config.auth.auth_jwt import get_current_user
 from app.config.auth.auth_password import verify_password
+from app.crud.sessions_crud import delete_session_by_user_id
 from app.crud.users_crud import get_user_by_email
 from app.database.postgresql import get_db
 from app.schemas.base_response import BaseResponse
@@ -59,3 +61,34 @@ async def login(
         )
 
     return BaseResponse(status=200, message="Login successful")
+
+
+@router.post(path="/logout")
+async def logout(
+    response: Response,
+    db: Session = Depends(get_db),
+    user_id: str = Depends(get_current_user),
+) -> BaseResponse:
+    """
+    ## User Logout
+    This endpoint allows an authenticated user to log out.
+
+    - **Deletes the user's session from the database**
+    - **Clears authentication cookies**
+
+    ### Responses:
+    - **200**: Successful logout, returns the number of deleted sessions.
+    - **401**: Unauthorized if the user is not authenticated.
+    """
+    with db:
+        delete_count = delete_session_by_user_id(db=db, user_id=user_id)
+
+    response.delete_cookie("access_token")
+    response.delete_cookie("refresh_token")
+    response.delete_cookie("token_type")
+
+    return BaseResponse(
+        status=200,
+        message="Logout successful",
+        data={"deleted_sessions": delete_count},
+    )
