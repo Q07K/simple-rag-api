4ff828a75dff76cfd038f22561144523f55bdaaa
gyuhyeong <kgh0730@gmail.com>
2025-03-15 14:30:07 +0900

    Feat: sessions get, delete 구현 추가

--- app/crud/sessions_crud.py
+++ app/crud/sessions_crud.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
 
+from sqlalchemy import delete, select
 from sqlalchemy.orm import Session
 
 # pylint:disable=wildcard-import, unused-wildcard-import
 from app.models import *
+from app.models.session_model import SessionModel
 
 
 def create_session(
@@ -19,4 +21,22 @@ def create_session(
     )
     db.add(model)
     db.commit()
-    db.close()
+
+
+def get_session_by_user_id(
+    db: Session,
+    user_id: str,
+) -> SessionModel | None:
+    stmp = select(SessionModel).where(SessionModel, user_id == user_id)
+    return db.execute(stmp).scalar_one()
+
+
+def delete_session_by_user_id(
+    db: Session,
+    user_id: str,
+):
+    smtp = delete(SessionModel).where(SessionModel.user_id == user_id)
+    result = db.execute(statement=smtp)
+    db.commit()
+
+    return result.rowcount
