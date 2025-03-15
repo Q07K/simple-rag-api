f9dfb5a9f7588be383de08d50bc1af5f8b771209
gyuhyeong <kgh0730@gmail.com>
2025-03-15 15:55:25 +0900

    Feat: 이미 사용중인 사용자인지 확인하는 로직 추가

--- app/routers/admin_router.py
+++ app/routers/admin_router.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.config import env
 from app.config.auth.auth_password import generate_password, get_password_hash
-from app.crud.users_crud import create_user
+from app.crud.users_crud import create_user, get_user_by_email
 from app.database.postgresql import get_db
 from app.schemas.base_response import BaseResponse
 from app.schemas.token_request import AdminToken
@@ -32,17 +32,19 @@ async def invite(
     - **Sends an invitation email** to the user with the password.
 
     ### Responses:
-    - `200`: Successfully invited the user and sent an email.
-    - `401`: Invalid password.
-    - `400`: If the input data is invalid, like missing or incorrect fields.
+    - **200**: Successfully invited the user and sent an email.
+    - **401**: Invalid password.
+    - **400**: If the input data is invalid, like missing or incorrect fields.
     """
     if body.token != env.SECRET_KEY:
         raise HTTPException(status_code=401, detail="Invalid password")
 
-    init_password = generate_password()
-    hashed_password = get_password_hash(password=init_password)
-
     with db:
+        if get_user_by_email(db=db, email=body.email):
+            raise HTTPException(status_code=409, detail="User already exists")
+
+        init_password = generate_password()
+        hashed_password = get_password_hash(password=init_password)
         create_user(db=db, email=body.email, hashed_password=hashed_password)
 
     message = send_invite_email(
