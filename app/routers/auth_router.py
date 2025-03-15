413dc218f868a24f11c672525a05fe1ea1a2fae4
gyuhyeong <kgh0730@gmail.com>
2025-03-15 14:28:27 +0900

    Feat: token 반환을 response body에서 cookies 로 관리하도록 수정

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,12 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from app.config.auth.security import verify_password
+from app.config.auth.auth_password import verify_password
 from app.crud.users_crud import get_user_by_email
 from app.database.postgresql import get_db
 from app.schemas.base_response import BaseResponse
-from app.schemas.security_schema import TokenModel
 from app.services.token_service import create_tokens
 
 router = APIRouter(
@@ -17,11 +16,12 @@ router = APIRouter(
 
 @router.post(path="/login")
 async def login(
+    response: Response,
     db: Session = Depends(get_db),
     form_data: OAuth2PasswordRequestForm = Depends(),
 ) -> BaseResponse:
     """
-    ## User Login API
+    ## User Login
     - **email**: User's email (used as the username in OAuth2 form)
     - **password**: User's password
 
@@ -29,27 +29,33 @@ async def login(
     - `200`: Login successful, returns access & refresh tokens.
     - `401`: Invalid email or password.
     """
-    model = get_user_by_email(db=db, email=form_data.username)
+    with db:
+        model = get_user_by_email(db=db, email=form_data.username)
 
-    is_verify_password = verify_password(
-        plain_password=form_data.password,
-        hashed_password=model.hashed_password,
-    )
-
-    if not is_verify_password:
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid email or password",
+        is_verify_password = verify_password(
+            plain_password=form_data.password,
+            hashed_password=model.hashed_password,
         )
 
-    access_token, refresh_token = create_tokens(db, model)
+        if not is_verify_password:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid email or password",
+            )
 
-    return BaseResponse(
-        status=200,
-        message="Login successful",
-        data=TokenModel(
-            access_token=access_token,
-            refresh_token=refresh_token,
-            token_type="bearer",
-        ),
-    )
+        access_token, refresh_token = create_tokens(db, model)
+
+    response_dict = {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "Bearer",
+    }
+    for response_key, response_value in response_dict.items():
+        response.set_cookie(
+            key=response_key,
+            value=response_value,
+            httponly=True,
+            samesite="Strict",
+        )
+
+    return BaseResponse(status=200, message="Login successful")
